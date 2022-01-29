5c680ba7de26363707d757c7993b2965394289d1
Martin Jablecnik <martin.jablecnik@email.cz>
2022-01-29 16:42:24 +0100

    Remove loadmore

--- lib/logs_widget.dart
+++ lib/logs_widget.dart
@@ -1,6 +1,5 @@
 import 'package:flogs/flogs.dart';
 import 'package:flutter/widgets.dart';
-import 'package:loadmore/loadmore.dart';
 
 class LogsWidget extends StatefulWidget {
   const LogsWidget({Key? key, this.from, this.to}) : super(key: key);
@@ -15,6 +14,12 @@ class LogsWidget extends StatefulWidget {
 class _LogsWidgetState extends State<LogsWidget> {
   List<Log> list = [];
 
+  @override
+  initState() {
+    super.initState();
+    load();
+  }
+
   Future<void> load() async {
     final logs = await FLog.logs.getByFilter(LogFilter(
       startDateTime: widget.from ?? DateTime(2020),
@@ -27,25 +32,16 @@ class _LogsWidgetState extends State<LogsWidget> {
 
   @override
   Widget build(BuildContext context) {
-    return LoadMore(
-      isFinish: list.isNotEmpty,
-      onLoadMore: _loadMore,
-      textBuilder: DefaultLoadMoreTextBuilder.english,
-      child: ListView.builder(
-        itemBuilder: (context, index) {
-          return Container(
-            child: Text(list[index].toString()),
-            height: 65.0,
-            alignment: Alignment.centerLeft,
-          );
-        },
-        itemCount: list.length,
-      ),
+    return ListView.builder(
+      padding: EdgeInsets.only(top: 24, left: 16, right: 16),
+      itemBuilder: (context, index) {
+        return Container(
+          child: Text(list[index].toString()),
+          height: 65.0,
+          alignment: Alignment.centerLeft,
+        );
+      },
+      itemCount: list.length,
     );
-  }
-
-  Future<bool> _loadMore() async {
-    load();
-    return true;
   }
 }
